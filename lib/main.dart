08b1177d1edd4a660f9f73f788934928d058ad82
maramkhateeb22 <marookhateeb@gmail.com>
2024-04-27 18:40:56 +0300

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:smile_shope_dash_board/HomeDashboard/SmileShop_Dashboard.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'Features/auth/presentation/views/homw_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
+final GoRouter _router = GoRouter(
+  initialLocation: '/home',
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/home',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeView();
+      }
+    ),
+  ],
+);
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,9 +25,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("ar", "AE"), // OR Locale('ar', 'AE') OR Other RTL locales
+      ],
+      locale: const Locale("ar", "AE"),
+routerConfig: _router,
 
-      home: ,
     );
   }
 }
